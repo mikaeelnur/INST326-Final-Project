b53024f268b4a6ec3320be47660ae51a3b7c5f07
khatudinh <khatudinh18@gmail.com>
2024-11-23 13:03:03 -0500

    Update finalproj.py

---
+++ finalproj.py
@@ -0,0 +1,18 @@
+class Habit:
+    def __init__(self, name, goal_frequency):
+        """ Initialize name, goal_frequency, current_streak, longest_streak, and last_logged_date."""
+        
+    def add_habit (habit_list, name, goal_frequency):
+        """ Creates a new habit object with the name and goal frequency, and adds it to the list.
+        Parameters: 
+        habit_list (list): The list to which the new habit object will be added. 
+        name (str): The name of the habit. 
+        goal_frequency (int): The goal frequency for the habit, how often the habit should be completed. 
+        Returns: None """
+        
+    def delete_habit (habit_list, habit_name):
+        """ Finds a habit by name in the habit list and removes it.
+        Parameters: 
+        habit_list (list): The list of habits. 
+        habit_name (str): The name of the habit to be removed. 
+        Returns: None """
