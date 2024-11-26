80e44a864680f1942eef95efb5fe0fb2adc67692
khatudinh <khatudinh18@gmail.com>
2024-11-25 21:26:14 -0500

    code under methods
    created another class, and put code under the first half of the methods

--- finalproj.py
+++ finalproj.py
@@ -1,21 +1,41 @@
+import datetime
+
 class Habit:
     def __init__(self, name, goal_frequency):
         """ Initialize name, goal_frequency, current_streak, longest_streak, and last_logged_date."""
-        
-    def add_habit (habit_list, name, goal_frequency):
+        self.name = name
+        self.goal_frequency= goal_frequency
+        self.current_streak= 0
+        self.longest_streak= 0
+        self.last_logged_date= None
+
+class HabitTracker:
+    def __init__(self):
+        """ Initializes the habit tracker object with an empty list of habits."""
+        self.habit_list = []
+
+    def add_habit (self, name, goal_frequency):
         """ Creates a new habit object with the name and goal frequency, and adds it to the list.
-        Parameters: 
-        habit_list (list): The list to which the new habit object will be added. 
+        Parameters:  
         name (str): The name of the habit. 
         goal_frequency (int): The goal frequency for the habit, how often the habit should be completed. 
         Returns: None """
-        
-    def delete_habit (habit_list, habit_name):
+
+        new_habit = Habit(name, goal_frequency)
+        self.habit_list.append(new_habit)
+        print(f"Habit '{name}' added succesfully.")
+
+    def delete_habit (self, habit_name):
         """ Finds a habit by name in the habit list and removes it.
         Parameters: 
         habit_list (list): The list of habits. 
         habit_name (str): The name of the habit to be removed. 
         Returns: None """
+        for habit in self.habit_list:
+            if habit.name== habit_name:
+                self.habit_list.remove(habit)
+                print(f"Habit '{habit_name}' successfully removed.")
+                return
 
     def log_progress (habit_name, date_logged):
         """Logs the progress of a habit by updating its streak based on the date logged
